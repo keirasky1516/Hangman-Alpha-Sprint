d8782eba5b31f276b3a5cd9981c1bfc6a362bf95
Keira <keira.plybon.2021@student.lhschools.org>
2018-11-26 08:09:30 -0500

    beta

--- src/HangmanAlphaSprint.java
+++ src/HangmanAlphaSprint.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
 public class HangmanAlphaSprint {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    Scanner keyboard=new Scanner(System.in);
-    int numberoftries=8;
-    String player2;
-    String input;
-    String secretWord = new String();
-    String playerGuess;
-    int wrongGuess;
-    wrongGuess=numberoftries;
+        Scanner keyboard = new Scanner(System.in);
+        int numberoftries = 8;
+        String player2;
+        String input;
+        String secretWord = new String();
+        String playerGuess;
+        int wrongGuess;
+        wrongGuess = numberoftries;
+
        /* for(int i = 0; i<secretWord.length(); i++) {
         } String a,b,c,d,e,f,g,h,i,j,k,l,m,n,p,q,r,s,t,u,v,w,x,y,z;*/
-    System.out.println("Welcome to hangman, would you like to play?");
-    keyboard.nextLine();
-    System.out.println("Great! Player 1 will guess the word chosen by player 2 in 8 or less guesses. If player one guesses" +
-            "the word correctly then they win, but if not player 2 wins.");
-    System.out.println("please enter a word player 2");
-        secretWord=keyboard.nextLine();
+        System.out.println("Welcome to hangman, would you like to play?");
+        keyboard.nextLine();
+        System.out.println("Great! Player 1 will guess the word chosen by player 2 in 8 or less guesses. If player one guesses" +
+                "the word correctly then they win, but if not player 2 wins.");
+        System.out.println("please enter a word player 2");
+        secretWord = keyboard.nextLine();
         System.out.println("Thank you! Now player 1 guess a letter");
         playerGuess = keyboard.nextLine();
-        
 
-        System.out.println("Guess is correct: "+ checkLetter(playerGuess,secretWord));
 
-        if(wrongGuess==1);
+        System.out.println("Guess is correct: " + checkLetter(playerGuess, secretWord));
+
+        if (wrongGuess == 1) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" |");
         System.out.println(" |");
@@ -40,45 +41,45 @@ public class HangmanAlphaSprint {
         System.out.println(" |");
 
         System.out.println("7 guesses left. Guess another letter");
-        playerGuess=keyboard.nextLine();
+        playerGuess = keyboard.nextLine();
 
-         if(wrongGuess==2);
-            System.out.println("Sorry that letter is not found");
-            System.out.println(" -------------------");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
+        if (wrongGuess == 2) ;
+        System.out.println("Sorry that letter is not found");
+        System.out.println(" -------------------");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
 
-            System.out.println("6 guesses left. Guess another letter");
-            playerGuess=keyboard.nextLine();
+        System.out.println("6 guesses left. Guess another letter");
+        playerGuess = keyboard.nextLine();
 
-            if(wrongGuess==3);
-            System.out.println("Sorry that letter is not found");
-            System.out.println(" -------------------");
-            System.out.println(" |                 (   )   ");
-            System.out.println(" |               (       )  ");
-            System.out.println(" |                 (   )    ");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
-            System.out.println(" |");
+        if (wrongGuess == 3) ;
+        System.out.println("Sorry that letter is not found");
+        System.out.println(" -------------------");
+        System.out.println(" |                 (   )   ");
+        System.out.println(" |               (       )  ");
+        System.out.println(" |                 (   )    ");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
+        System.out.println(" |");
 
-            System.out.println("5 guesses left. Try again");
-            playerGuess=keyboard.nextLine();
+        System.out.println("5 guesses left. Try again");
+        playerGuess = keyboard.nextLine();
 
-            if(wrongGuess==4);
+        if (wrongGuess == 4) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" -------------------");
         System.out.println(" |                 (   )   ");
@@ -94,9 +95,9 @@ public class HangmanAlphaSprint {
         System.out.println(" |                           ");
 
         System.out.println("4 guesses left. Try again");
-        playerGuess=keyboard.nextLine();
+        playerGuess = keyboard.nextLine();
 
-        if(wrongGuess==5);
+        if (wrongGuess == 5) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" -------------------");
         System.out.println(" |                 (   )   ");
@@ -112,9 +113,9 @@ public class HangmanAlphaSprint {
         System.out.println(" |                 (          ");
 
         System.out.println("3 guesses left. Try again");
-        playerGuess=keyboard.nextLine();
+        playerGuess = keyboard.nextLine();
 
-        if(wrongGuess==6);
+        if (wrongGuess == 6) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" -------------------");
         System.out.println(" |                 (   )   ");
@@ -130,9 +131,9 @@ public class HangmanAlphaSprint {
         System.out.println(" |                 (   )       ");
 
         System.out.println("2 guesses left. Try again");
-        playerGuess=keyboard.nextLine();
+        playerGuess = keyboard.nextLine();
 
-        if(wrongGuess==7);
+        if (wrongGuess == 7) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" -------------------");
         System.out.println(" |                 (   )   ");
@@ -148,9 +149,9 @@ public class HangmanAlphaSprint {
         System.out.println(" |                 (   )       ");
 
         System.out.println("1 guess left. Try again");
-        playerGuess=keyboard.nextLine();
+        playerGuess = keyboard.nextLine();
 
-        if(wrongGuess==8);
+        if (wrongGuess == 8) ;
         System.out.println("Sorry that letter is not found");
         System.out.println(" -------------------");
         System.out.println(" |                 (   )   ");
@@ -168,16 +169,16 @@ public class HangmanAlphaSprint {
         System.out.println("Game over. Player 2 wins.");
 
 
-
-
-
-        }
+    }
         public static boolean checkLetter(String letter, String secretWord){
 
         if(secretWord.contains(letter)){
             return true;
         }
         return false;
-}
 
-}
+
+
+
+
+}}
